12b5ece60ac714c5f876e3ea7c975171a7f1ab62
vikramgrewal <vik.grewal1992@gmail.com>
2018-04-12 13:33:55 -0700

    Removed merge conflicts in view controller and storyboard.

--- Pocket Fitness/Controllers/LoginViewController.swift
+++ Pocket Fitness/Controllers/LoginViewController.swift
@@ -67,11 +67,8 @@ class LoginViewController: UIViewController {
       let alertVC = UIAlertController(title: nil, message: nil, preferredStyle: .alert)
 
       if UserSession.isLoggedIn()   {
-<<<<<<< HEAD
          alertVC.title = "Logged In"
-=======
          print("Logged in")
->>>>>>> 32e5c320868ed4fc8545d727eb5cf647efd991a9
       }  else  {
          alertVC.title = "Not logged in"
       }
